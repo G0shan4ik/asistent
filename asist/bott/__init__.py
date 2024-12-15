986ba32ff3b5c3b726f0ae07369baf4d6ee5390b
Egor <egorkononec64@gmail.com>
2024-12-15 18:14:25 +0300

    [m] init project

---
+++ asist/bott/__init__.py
@@ -0,0 +1,1 @@
+from . import core, greet, commands, set_handlers, states
---
+++ main.py
@@ -0,0 +1,22 @@
+import asyncio
+
+import sys
+import logging
+
+from asist.database import init
+from asist.bott.core import start_bot
+
+from loguru import logger
+
+
+def start_dev() -> None:
+    logger.success('Start APP')
+    logging.basicConfig(level=logging.INFO, stream=sys.stdout)
+
+    init()
+    logger.info('<-- Init DB -->')
+    try:
+        asyncio.run(start_bot())
+        logger.info('<-- Start BOT -->')
+    except (asyncio.exceptions.CancelledError, KeyboardInterrupt):
+        logger.warning('The bot was forcibly stopped')
