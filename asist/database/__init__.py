a485cf37577be564424b8387ed28ec35ee29419b
Egor <egorkononec64@gmail.com>
2024-12-19 23:03:48 +0300

    [-] added new currency (EUR) fo db

--- asist/database/__init__.py
+++ asist/database/__init__.py
@@ -29,6 +29,7 @@ class CoursesCurrencies(BaseModel):
     dollar_ruble_by = BigIntegerField(null=True)
     dollar_ruble_ru = BigIntegerField(null=True)
     rouble_ru_by = BigIntegerField(null=True)
+    eu_ru_by = BigIntegerField(null=True)
 
     update_time = DateTimeField(null=True)
 
